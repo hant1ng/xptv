afa73719495fdfa37262e6e149ed1a21e1781aa8
日他妈叼 <134476776+hant1ng@users.noreply.github.com>
2025-09-24 12:23:29 +0800

    Update wanou.js

--- wanou.js
+++ wanou.js
@@ -1,7 +1,7 @@
 const cheerio = createCheerio()
 const CryptoJS = createCryptoJS()
 
-const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
 
 let appConfig = {
     ver: 1,
